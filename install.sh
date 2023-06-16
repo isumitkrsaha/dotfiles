3fc04cb4b01765f6065c9a772310b127976da592
isumitkrsaha <125203141+isumitkrsaha@users.noreply.github.com>
2023-06-16 21:33:26 +0530

    second commit with wget

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 echo "Downloading Essestial Dotfiles"
 
 URL="https://raw.githubusercontent.com/isumitkrsaha/dotfiles/main/"
-OUT="~/"
 
 echo "== bash helper"
-wget --output-document $OUT.bash_helper $URL.bash_helper
-wget --output-document $OUT.inputrc $URL.inputrc
+wget --output-document $HOME.bash_helper $URL.bash_helper
+wget --output-document $HOME.inputrc $URL.inputrc
 
 echo "== git helper"
-wget --output-document $OUT.gitconfig $URL.gitconfig
-wget --output-document $OUT.gitignore_global $URL.gitignore_global
-wget --output-document $OUT.gitmessage.txt $URL.gitmessage.txt
+wget --output-document $HOME.gitconfig $URL.gitconfig
+wget --output-document $HOME.gitignore_global $URL.gitignore_global
+wget --output-document $HOME.gitmessage.txt $URL.gitmessage.txt
 
 echo "== vim helper"
-wget --output-document $OUT.vimrc $URL.vimrc
+wget --output-document $HOME.vimrc $URL.vimrc
 
 echo "== python helper"
-wget --output-document $OUT.condarc $URL.condarc
-wget --output-document $OUT.pylintrc $URL.pylintrc
+wget --output-document $HOME.condarc $URL.condarc
+wget --output-document $HOME.pylintrc $URL.pylintrc
