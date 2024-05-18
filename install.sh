a3151e738c62a5f8900c234d2727c23998338582
Sumit Kr Saha <125203141+isumitkrsaha@users.noreply.github.com>
2024-05-19 01:03:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-echo "Downloading Essestial Dotfiles"
+echo "Downloading Essential Dotfiles"
 
 URL="https://raw.githubusercontent.com/isumitkrsaha/dotfiles/main"
 
@@ -16,20 +16,6 @@ wget --output-document $HOME/.tmux.conf $URL/.tmux.conf
 
 echo "== vim helper"
 wget --output-document $HOME/.vimrc $URL/.vimrc
-git clone --depth 1 https://tpope.io/vim/sensible.git ~/.vim/pack/tpope/start/sensible
-git clone --depth 1 https://tpope.io/vim/sleuth.git ~/.vim/pack/tpope/start/sleuth
-vim -u NONE -c "helptags sleuth/doc" -c q
-git clone --depth 1 https://tpope.io/vim/commentary.git ~/.vim/pack/tpope/start/commentary
-vim -u NONE -c "helptags commentary/doc" -c q
-git clone --depth 1 https://github.com/tpope/vim-flagship.git ~/.vim/pack/tpope/start/vim-flagship
-vim -u NONE -c "helptags vim-flagship/doc" -c q
-git clone --depth 1 https://github.com/sheerun/vim-polyglot ~/.vim/pack/plugins/start/vim-polyglot
-git clone --depth 1 https://github.com/Raimondi/delimitMate ~/.vim/pack/plugins/start/delimitMate
-git clone --depth 1 https://tpope.io/vim/endwise.git ~/.vim/pack/tpope/start/endwise
-git clone --depth 1 https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
-vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
-git clone --depth 1 https://github.com/tpope/vim-vinegar.git ~/.vim/pack/tpope/start/vim-vinegar
-git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
 
 echo "== python helper"
 wget --output-document $HOME/.condarc $URL/.condarc
